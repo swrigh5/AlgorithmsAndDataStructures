a6399b4efc3cae624326b7c70c26c43d1669ead9
Stephen Wright <Stephen.Wright@amtrustgroup.com>
2019-01-02 16:52:40 -0500

    get and set

---
+++ AlgorithmsAndDataStructures/ArrayTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AlgorithmsAndDataStructures
+{
+    [TestFixture]
+    class ArrayTests
+    {
+        private DynamicArray array;
+
+        [SetUp]
+        public void SetUp()
+        {
+            array = new DynamicArray(2);
+        }
+
+
+        [Test]
+        public void GetAndSet()
+        {
+            array.Set(0, "a");
+            Assert.That(array.Get(0), Is.EqualTo("a"));
+        }
+
+
+    }
+}
---
+++ AlgorithmsAndDataStructures/DynamicArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures
+{
+    public class DynamicArray
+    {
+        private object[] data;
+        private int size;
+        private int initialCapacity;
+
+        public DynamicArray(int initialCapacity)
+        {
+            this.initialCapacity = initialCapacity;
+            data = new object[initialCapacity];
+        }
+
+        public string Get(int index)
+        {
+            return (string)data[index];
+        }
+
+        public void Set(int index, string value)
+        {
+            data[index] = value;
+        }
+        
+    }
+}
